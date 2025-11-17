21d8323af19976392416bca0c75e61528721177a
Axsell Eker <axsell@live.com>
2025-11-17 20:37:00 -0300

    Insercao de classe personagem

---
+++ personagem.py
@@ -0,0 +1,7 @@
+class Personagem:
+    def __init__(self, nome, idade, altura, peso, cor):
+        self.nome = nome
+        self.idade = idade
+        self.altura = altura
+        self.peso = peso
+        self.cor = cor
